87e5b1c84548ef806476c5bc3d5ec0e1f8e003ce
Douglas Mariano <douglas.mariano@estudante.ufjf.br>
2025-09-23 22:13:51 -0300

    Criado Disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Disciplina from './Disciplina';
 function App() {
   return (
     <>
       <h1>Olá Mundo!</h1>
       <h2>teste</h2>
+      <Disciplina />
     </> 
   );
 }
